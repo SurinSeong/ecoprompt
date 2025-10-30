35f0ae54d340eea2571804ca2c7d68c8f9891f00
surin <tjdtnfls1128@gmail.com>
2025-10-30 10:41:28 +0900

    feat: llm 로더 파일 수정중

--- app/models/llm_loader.py
+++ app/models/llm_loader.py
@@ -1,28 +1,40 @@
-from vllm import SamplingParams, AsyncEngineArgs, AsyncLLMEngine
-from transformers import AutoTokenizer
+import asyncio
+from vllm import LLM, SamplingParams
+from vllm.v1.engine.async_llm import AsyncLLM
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.sampling_params import RequestOutputKind
 
 from app.core.config import base_settings
 
-# 토크나이저 로딩
-tokenizer = AutoTokenizer.from_pretrained(
-    "sh2orc/Llama-3.1-Korean-8B-Instruct",
-    trust_remote_code=True
-)
+_sync_llm = None
 
-# LLM 엔진 설정
-engine_args = AsyncEngineArgs(
+def load_sync_llm():
+    """sync llm 로드"""
+    
+    pass
+
+# LLM 설정
+llm = LLM(
     model="./llm-models/Llama-3.1-Korean-8B-Instruct",    # "sh2orc/Llama-3.1-Korean-8B-Instruct",
+    tokenizer="meta-llama/Llama-3.1-8B-Instruct",
+    trust_remote_code=True,
+    dtype="auto",
     gpu_memory_utilization=base_settings.gpu_memory_utilization,
-    tensor_parallel_size=base_settings.tensor_parallel_size,
     max_model_len=base_settings.max_model_len,
-    max_num_seqs=base_settings.max_num_seqs
 )
 
-llm = AsyncLLMEngine.from_engine_args(engine_args)
+# AsyncLLM 설정
+engine_args = AsyncEngineArgs(
+    model="meta-llama/Llama-3.1-8B-Instruct",
+    enforce_eager=True,
+)
+
+async_llm = AsyncLLM.from_engine_args(engine_args)
 
 sampling_params = SamplingParams(
     temperature=base_settings.temperature,    # 답변의 다양성
     top_p=base_settings.top_p,    # 핵심 단어 생성 기준
     max_tokens=base_settings.max_tokens,    # 제한 토큰 수
-    stop=["<eot_id>"] 
+    seed=42,
+    output_kind=RequestOutputKind.DELTA,
 )
